3121cb3b9d57d04974b73d2add677949c52b1eec
vanshslathia <vanshslathia7051@gmail.com>
2026-06-01 02:12:58 +0530

    final push

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,10 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: true,
+    origin: [
+        "http://localhost:5173",
+        "https://ai-interview-preparation-navy.vercel.app"
+    ],
     credentials: true
 }))
 
